3720f700ccd7373a2405beec6a1d4606b299f454
rokondo <rokdd@web.de>
2011-07-10 16:38:24 +0200

    Rewrite of some listeners and vars
    Signed-off-by: rokondo <rokdd@web.de>

--- chrome/content/zoteroautoexporting/hello.js
+++ chrome/content/zoteroautoexporting/hello.js
@@ -1,103 +1,138 @@
+	
+if ("undefined" == typeof(Zotero)) {
+	var Zotero = Components.classes["@zotero.org/Zotero;1"].getService(Components.interfaces.nsISupports).wrappedJSObject;
+}
+if("undefined" == typeof(Zotero.AutoExporting))
+	{
 	Zotero.AutoExporting = {
 		boolLog: true,
 		boolActivated:true,
 		prefManager:null,
-		notifierID :null,
 		active:null,
 		fileintInterval: 1,
 		filepath:null,
 		filetranslator:null,
+		observerPref:null,
 		fileTimer:null,
+		Prefs:{
+			},
 		init:function()
 			{
+			//init the pref observer to get a notice when prefs changed and timer can be reseted
+			this.observerPref={
+				  register: function() {
+					var prefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+					this._branch = prefService.getBranch("extensions.zoteroautoexporting.");
+					this._branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+					this._branch.addObserver("", this, false);
+				  },
+				  unregister: function() {
+					if (!this._branch) return;
+					this._branch.removeObserver("", this);
+				  },
+				  observe: function(aSubject, aTopic, aData) {
+					if(aTopic = "nsPref:changed")
+					{
+					Zotero.AutoExporting.reload();
+					}
+				  }
+				}
+			this.observerPref.register();
+			
 			this.renew();
 			},
 		reload:function()
 			{
-			clearTimeout(this.fileTimer);
-			this.log('cleared timeout');
+			this.fileTimer.cancel();
+			this.log('Timer is being canceled');
 			this.renew();
 			},
 		renew: function ()
 			{
 			this.prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-				//check whether we show debugs or not
-				var temp = this.prefManager.getBoolPref("extensions.zoteroautoexporting.showdebug");
-				if(temp==true)
-					{
-					this.boolLog=true;
-					}
-				var temp = this.prefManager.getBoolPref("extensions.zoteroautoexporting.filedeactivate");
-				if(temp==true)
-					{
-					this.boolActivated=false;
-					this.log('Autoexporting for files is disbaled');
-					}
-				var temp = this.prefManager.getCharPref("extensions.zoteroautoexporting.filepath");
+			
+			//check whether we show debugs or not
+			this.boolLog= this.prefManager.getBoolPref("extensions.zoteroautoexporting.showdebug");
+			
+	
+			this.boolActivated = this.prefManager.getBoolPref("extensions.zoteroautoexporting.filedeactivate");
+			if(this.boolActivated==false)
+				{
+				this.log('Autoexporting for files is disbaled');
+				}
+			
+			
+			var temp = this.prefManager.getCharPref("extensions.zoteroautoexporting.filepath");
+			if(temp.length>4)
+				{
+				this.log('Filepath seems good');
+				this.filepath=temp;
+				}
+			else
+				{
+				this.boolActivated=false;
+				this.log('Looks not like a valid path.. Sorry');
+				}
 				
-				if(temp.length>4)
-					{
-					this.log('Filepath seems good');
-					this.filepath=temp;
-					}
-				else
-					{
-					this.boolActivated=false;
-					this.log('Looks not like a valid path.. Sorry');
-					}
-				var temp = this.prefManager.getCharPref("extensions.zoteroautoexporting.filetranslator");
+			var temp = this.prefManager.getCharPref("extensions.zoteroautoexporting.filetranslator");
+			if(temp.length>4)
+				{
+				this.log('Filetranslator seems good');
+				this.filetranslator=temp;
+				}
+			else
+				{
+				this.boolActivated=false;
+				this.log('Looks not like a valid filetranslator.. Sorry');
+				}
 				
-				if(temp.length>4)
-					{
-					this.log('Filetranslator seems good');
-					this.filetranslator=temp;
-					}
-				else
-					{
-					this.boolActivated=false;
-					this.log('Looks not like a valid filetranslator.. Sorry');
-					}
-				var temp = this.prefManager.getIntPref("extensions.zoteroautoexporting.fileinterval");
-				if(temp<1)
-					{
-					this.boolActivated=false;
-					this.log('Looks not like a valid interval.. ');
-					return false;
-					}
-				else
-					{
-					this.fileintInterval=temp;
-					}
-				if(this.boolActivated==false)
-					{
-					this.log('Abort init process and set no timer');
-					return false;
-					}
-				else
-					{
-					this.fileTimer=setTimeout(function () { Zotero.AutoExporting.autoexport(); } , this.fileintInterval * 60 * 1000);
-					}
-				this.log('init done');
-		
+			var temp = this.prefManager.getIntPref("extensions.zoteroautoexporting.fileinterval");
+			if(temp<1)
+				{
+				this.boolActivated=false;
+				this.log('Looks not like a valid interval.. ');
+				return false;
+				}
+			else
+				{
+				this.fileintInterval=temp;
+				}
+				
+			if(this.boolActivated==false)
+				{
+				this.log('Abort init process and set no timer');
+				return false;
+				}
+			else
+				{
+				this.fileexportTimer();
+				}
+			this.log('Renew done');
 			},
+			
+		fileexportTimer: function ()
+			{
+			this.log('Set the timer for next export in '+this.fileintInterval +' minutes');
+			this.fileTimer= Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
+			this.fileTimer.initWithCallback(function () { Zotero.AutoExporting.autoexport(); },this.fileintInterval * 60 * 1000, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
+			}
+		,
 		autoexport: function ()
 			{
-			this.log('Run autoexporting');
-			//var translator = new Zotero.Translate.Web(); 
-
-				var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-				file.initWithPath(this.filepath);
-				
-				this.log('translator inits');
-				
-				var expTranslator = new Zotero.Translate('export');
-				expTranslator.setLocation(file);
-				expTranslator.setTranslator(this.filetranslator);
-				this.log('translator runs');
-				expTranslator.translate();
-				var currentTime = new Date();
-				this.prefManager.setCharPref("extensions.zoteroautoexporting.filestatus",'Exported  '+currentTime.getDate()+'.'+(currentTime.getMonth() + 1)+'.'+currentTime.getFullYear()+'  '+currentTime.getHours()+':'+currentTime.getMinutes());
-				this.fileTimer=setTimeout(function () { Zotero.AutoExporting.autoexport(); } , this.fileintInterval * 60 * 1000);
+			this.log('Run autoexporting to file');
+			var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+			file.initWithPath(this.filepath);
+			
+			this.log('Translator for export inits');
+			var expTranslator = new Zotero.Translate('export');
+			expTranslator.setLocation(file);
+			expTranslator.setTranslator(this.filetranslator);
+			this.log('Translatpr for export now run..');
+			expTranslator.translate();
+			
+			var currentTime = new Date();
+			this.prefManager.setCharPref("extensions.zoteroautoexporting.filestatus",'Exported  '+currentTime.getDate()+'.'+(currentTime.getMonth() + 1)+'.'+currentTime.getFullYear()+'  '+currentTime.getHours()+':'+currentTime.getMinutes());
+			this.fileexportTimer();
 			},
 		
 		log: function (msg) {
@@ -109,38 +144,6 @@
     }
 	};
 	
-if (!Zotero.AutoExporting) {
-	var Zotero = Components.classes["@zotero.org/Zotero;1"].getService(Components.interfaces.nsISupports).wrappedJSObject;
-
-}
-Zotero.AutoExporting.init();
-
-var myPrefObserver = {
-  register: function() {
-    // First we'll need the preference services to look for preferences.
-    var prefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-
-    // For this._branch we ask that the preferences for extensions.myextension. and children
-    this._branch = prefService.getBranch("extensions.zoteroautoexporting.");
-
-    // Now we queue the interface called nsIPrefBranch2. This interface is described as:  
-    // "nsIPrefBranch2 allows clients to observe changes to pref values."
-    this._branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
-
-    // Finally add the observer.
-    this._branch.addObserver("", this, false);
-  },
-
-  unregister: function() {
-    if (!this._branch) return;
-    this._branch.removeObserver("", this);
-  },
-
-  observe: function(aSubject, aTopic, aData) {
-    if(aTopic = "nsPref:changed")
-    {
-    Zotero.AutoExporting.reload();
-    }
-  }
-}
-myPrefObserver.register();
+	
+	}
+window.addEventListener("load", function() { Zotero.AutoExporting.init(); }, false);
--- chrome/content/zoteroautoexporting/include.js
+++
@@ -1,5 +0,0 @@
-// Only create main object once
-if (!Zotero.AutoExporting) {
-	var Zotero = Components.classes["@zotero.org/Zotero;1"].getService(Components.interfaces.nsISupports).wrappedJSObject;
-
-}
