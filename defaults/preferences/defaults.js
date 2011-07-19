7dbc8449be1c3bb5dc8b512d909abfd3ac093c2f
rokondo <rokdd@web.de>
2011-07-19 17:36:57 +0200

    restructering

--- defaults/preferences/defaults.js
+++
@@ -1,6 +0,0 @@
-﻿pref("extensions.zoteroautoexporting.filedeactivate", true);
-pref("extensions.zoteroautoexporting.showdebug", false);
-pref("extensions.zoteroautoexporting.fileinterval", 120);
-pref("extensions.zoteroautoexporting.filetranslator", '9cb70025-a888-4a29-a210-93ec52da40d4');
-pref("extensions.zoteroautoexporting.filepath", 'X:\\to\\folder\\test.bib');
-pref("extensions.zoteroautoexporting.filestatus", 'No actions done');
