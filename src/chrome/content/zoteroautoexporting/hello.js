0db4e0dc55d66fcbe601c74ba584a609a06e964d
rokondo <rokdd@web.de>
2011-08-18 22:24:46 +0200

    version 0.6 with file picker now

--- src/chrome/content/zoteroautoexporting/hello.js
+++ src/chrome/content/zoteroautoexporting/hello.js
@@ -1,8 +1,16 @@
 	
 if ("undefined" == typeof(Zotero)) {
-	var Zotero = Components.classes["@zotero.org/Zotero;1"].getService(Components.interfaces.nsISupports).wrappedJSObject;
+	if (!("@zotero.org/Zotero;1" in Components.classes))
+		{
+		EmergencyPrefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+		EmergencyPrefManager.setCharPref("extensions.zoteroautoexporting.filestatus",'No Zotero Addon was found. Please download from zotero.org');
+		}
+	else
+		{
+		var Zotero = Components.classes["@zotero.org/Zotero;1"].getService(Components.interfaces.nsISupports).wrappedJSObject;
+		}
 }
-if("undefined" == typeof(Zotero.AutoExporting))
+if("undefined" != typeof(Zotero) && "undefined" == typeof(Zotero.AutoExporting))
 	{
 	Zotero.AutoExporting = {
 		boolLog: false,
@@ -15,6 +23,7 @@ if("undefined" == typeof(Zotero.AutoExporting))
 		observerPref:null,
 		fileTimer:null,
 		filelast:null,
+		filenslfile:null,
 		file_keep:false,
 		Prefs:{
 			},
@@ -44,7 +53,7 @@ if("undefined" == typeof(Zotero.AutoExporting))
 				}
 			this.observerPref.register();
 			
-			this.renew();
+			this.renew('init addon');
 			 if(this.file_keep==true && (new Date().getTime() - this.filelast)>this.fileintInterval)
 				{
 				this.log('Fired export because last time is longer ago as interval is set');
@@ -55,10 +64,11 @@ if("undefined" == typeof(Zotero.AutoExporting))
 			{
 			this.fileTimer.cancel();
 			this.log('Timer is being canceled');
-			this.renew();
+			this.renew('reload after timer reset');
 			},
-		renew: function ()
+		renew: function (reason)
 			{
+			this.log('Start of renew because of: '+reason);
 			this.prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 			
 			//check whether we show debugs or not
@@ -70,6 +80,7 @@ if("undefined" == typeof(Zotero.AutoExporting))
 				this.log('Autoexporting for files is disabled by user');
 				}
 			
+			//thats the old option!
 			var temp = this.prefManager.getCharPref("extensions.zoteroautoexporting.filepath");
 			if(temp.length>4)
 				{
@@ -78,9 +89,31 @@ if("undefined" == typeof(Zotero.AutoExporting))
 				}
 			else
 				{
-				this.boolActivated=false;
 				this.log('Looks not like a valid path.. Sorry');
 				}
+			//now check the new one :)
+			if(this.prefManager.getBranch("extensions.zoteroautoexporting.").prefHasUserValue('extensions.zoteroautoexporting.filenslfile'))
+				{
+				this.filenslfile =this.prefManager.getBranch("extensions.zoteroautoexporting.").getComplexValue("extensions.zoteroautoexporting.filenslfile", Components.interfaces.nsILocalFile);
+				}
+			if(typeof(this.filenslfile)!='undefined' && this.filenslfile!=null && this.filenslfile.path.length>0)
+				{
+				this.log('Path of the new file options looks valid');
+				}
+			else if(this.filepath!=null && this.filepath.length>0)
+				{
+				//try to migrate from the old path!
+				this.filenslfile = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+				this.filenslfile.initWithPath(this.filepath);
+				this.prefManager.getBranch("extensions.zoteroautoexporting.").setComplexValue("extensions.zoteroautoexporting.filenslfile", Components.interfaces.nsILocalFile, this.filenslfile);
+				this.log('Migrated from old path to the new file object');
+				}
+			else
+				{
+				this.boolActivated=false;
+				this.log('Looks not like a valid path or file!');
+				}
+				
 				
 			var temp = this.prefManager.getCharPref("extensions.zoteroautoexporting.filetranslator");
 			if(temp.length>4)
@@ -118,44 +151,63 @@ if("undefined" == typeof(Zotero.AutoExporting))
 				{
 				this.fileexportTimer();
 				}
-			this.log('Renew done');
+			this.log('Renew  done (because of: '+reason+')');
 			},
 			
 		fileexportTimer: function ()
 			{
-			this.log('Set the timer for next export in '+this.fileintInterval +' minutes');
-			this.fileTimer= Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
-			this.fileTimer.initWithCallback(function () { Zotero.AutoExporting.autoexport(); },this.fileintInterval * 60 * 1000, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
+			
+			if(this.fileintInterval > 2)
+				{
+				this.fileTimer= Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
+				this.fileTimer.initWithCallback(function () { Zotero.AutoExporting.autoexport(); },this.fileintInterval * 60 * 1000, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
+				this.log('Set the timer for next export in '+this.fileintInterval +' minutes');
+				}
+			else
+				{
+				this.log('Interval is not correct or too less!');
+				}
 			}
 		,
 		autoexport: function ()
 			{
 			this.log('Run autoexporting to file');
-			var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-			file.initWithPath(this.filepath);
-			
-			this.log('Translator for export inits');
-			var expTranslator = new Zotero.Translate('export');
-			expTranslator.setLocation(file);
-			expTranslator.setTranslator(this.filetranslator);
-			this.log('Translatpr for export now run..');
-			expTranslator.translate();
-			
-			var currentTime = new Date();
-			this.prefManager.setCharPref("extensions.zoteroautoexporting.filestatus",'Exported  '+currentTime.getDate()+'.'+(currentTime.getMonth() + 1)+'.'+currentTime.getFullYear()+'  '+currentTime.getHours()+':'+currentTime.getMinutes());
-			this.prefManager.setIntPref("extensions.zoteroautoexporting.file-last",currentTime.getTime());
+			//var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+			//file.initWithPath(this.filepath);
+
+			//var this.filenslfile =this.prefManager.getComplexValue("extensions.zoteroautoexporting.filenslfile", Components.interfaces.nsILocalFile);
+			if(this.filenslfile.path.length>0)
+				{
+				this.log('Fileobject is writeable')
+				this.log('Translator for export inits');
+				var expTranslator = new Zotero.Translate('export');
+				expTranslator.setLocation(this.filenslfile);
+				expTranslator.setTranslator(this.filetranslator);
+				this.log('Translator for export now run..');
+				expTranslator.translate();
+				
+				var currentTime = new Date();
+				this.log('Exported  '+currentTime.getDate()+'.'+(currentTime.getMonth() + 1)+'.'+currentTime.getFullYear()+'  '+currentTime.getHours()+':'+(( currentTime.getMinutes() < 10 ? "0" : "" ) + currentTime.getMinutes()),true);
+				this.prefManager.setIntPref("extensions.zoteroautoexporting.file-last",currentTime.getTime());
+				}
+			else
+				{
+				this.log('Fileobject is not specified or not writeable');
+				}
 			this.fileexportTimer();
 			},
 		
-		log: function (msg) {
-		if(this.boolLog==true)
-			{
-			 var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
-		   consoleService.logStringMessage('[zoteroautoexporting] '+msg);
-		   }
-    }
+		log: function (msg, boolPanel) {
+			if(boolPanel==true)
+				{
+				this.prefManager.setCharPref("extensions.zoteroautoexporting.filestatus",msg);
+				}
+			if(this.boolLog==true)
+				{
+				 var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+			   consoleService.logStringMessage('[zoteroautoexporting] '+msg);
+			   }
+			}
 	};
-	
-	
 	}
 window.addEventListener("load", function() { Zotero.AutoExporting.init(); }, false);
--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -3,6 +3,7 @@ pref("extensions.zoteroautoexporting.showdebug", false);
 pref("extensions.zoteroautoexporting.fileinterval", 120);
 pref("extensions.zoteroautoexporting.filetranslator", '9cb70025-a888-4a29-a210-93ec52da40d4');
 pref("extensions.zoteroautoexporting.filepath", 'X:\\to\\folder\\test.bib');
+pref("extensions.zoteroautoexporting.filenslfile", '');
 pref("extensions.zoteroautoexporting.filestatus", 'No actions done');
 pref("extensions.zoteroautoexporting.file-last", 0);
 pref("extensions.zoteroautoexporting.file-bool-keep-interval", false);
